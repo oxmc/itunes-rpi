fd13538d6cb321753bf06b4aa40afe562df757ef
oxmc <67136658+oxmc@users.noreply.github.com>
2022-05-09 18:59:52 -0700

    Update getinfo.py

--- getinfo.py
+++ getinfo.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import time,json,os,getpass,sys,re
+import time,json,os,getpass,sys,re,base64
 from pathlib import Path
 from functions import fixjson,createjsonfile,istherefile,getmp3info
 
@@ -30,13 +30,12 @@ time.sleep(2)
 #Windows Testing:
 drive_name = "ALEKS"
 #Normal:
-#drive_name = os.popen(f"lsblk {devicejson['DEVPATH']} | tail -n +2 | cut -d/ -f4").read().strip()
+#drive_name = os.popen(f"lsblk {devicejson['DEVPATH']} | tail -n +2 | awk '{print $NF}' | cut -d/ -f4 | tr '\n' ' ' | awk '{print $NF}'").read().strip()
 print("Checking for mp3's")
 #Windows Testing:
 directory = f"D:\\iPod_Control\\Music\\"
 #Normal:
 #directory = f"/media/{username}/{drive_name}/iPod_Control/Music/"
-#print(directory)
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     # checking if it is a dir
@@ -52,26 +51,17 @@ for filename in os.listdir(directory):
                 minfo['artist'] = mi['artist']
                 minfo['album'] = mi['album']
                 minfo['title'] = mi['title']
-                #music_info = (minfo)
-                print(file)
+                minfo['b64'] = "error"
+                with open(f"{floc}", "rb") as audio_file:
+                    b64_string = base64.b64encode(audio_file.read())
+                    minfo['b64'] = b64_string
                 music_info1 = {minfo}
                 print(music_info1)
-                #music_info2 =re.sub(r'.', '', str(music_info1), count = 1)
-                #print(music_info2)
-                #music_info = music_info2[:-1]
-                #minfo = {file:{**music_info[file], **d}}
-                #music_info = f'{file}":"{music_info}'
-                print(music_info1)
                 media_list3.append(music_info1)
-                #media_list2[file] = music_info
                 #deviceinfo = os.popen(f'cat /media/{username}/{drive_name}/iPod_Control/iTunes/DeviceInfo').read().strip()
-            #else:
-            #    print("False")
     else:
         print(f"Not mp3: {f}")
 
-#print(media_list2)
-#media_list3.append(media_list2)
 print(media_list3)
 media_list["media"] = media_list3
 print(media_list)
